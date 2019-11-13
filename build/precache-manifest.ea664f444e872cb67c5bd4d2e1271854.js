25b2b869dc40dea747dd25c5d903f05fbf946a6c
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2019-11-13 07:45:38 -0800

    Add files via upload
    11/13/19: Initial commit

---
+++ build/precache-manifest.ea664f444e872cb67c5bd4d2e1271854.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f85789465faaf308191dd36c6c5896a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1fca440dec7f7dc0d22",
+    "url": "/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "ec3d1cdd9349ba138936",
+    "url": "/static/js/2.976e333b.chunk.js"
+  },
+  {
+    "revision": "f1fca440dec7f7dc0d22",
+    "url": "/static/js/main.175e9148.chunk.js"
+  },
+  {
+    "revision": "b17c5ae61281d5fd840e",
+    "url": "/static/js/runtime~main.37fd7420.js"
+  },
+  {
+    "revision": "437f801005e6191a5e3167c1913e6321",
+    "url": "/static/media/bg7.437f8010.jpg"
+  },
+  {
+    "revision": "f5171bb55ea599d1d19913c0d645d9d1",
+    "url": "/static/media/coffeecupPlain.f5171bb5.jpg"
+  },
+  {
+    "revision": "0183c735573561fd50dff976ba617d3a",
+    "url": "/static/media/darkroast.0183c735.jpg"
+  },
+  {
+    "revision": "30cfd59212cd1e1556dcc615827944c7",
+    "url": "/static/media/decaf.30cfd592.jpg"
+  },
+  {
+    "revision": "ea35b918561a31040ccf79073dc3855b",
+    "url": "/static/media/lg-logo.ea35b918.png"
+  },
+  {
+    "revision": "79b104e91d9f5960f2c2175f1548cd8d",
+    "url": "/static/media/mediumroast.79b104e9.jpeg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  }
+]);
